e553baf8d1523eae743ecf9fc515e72e50ee6e77
Cleveland <eclevela@richmond.edu>
2021-04-10 01:14:28 -0400

    summary table complete PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,22 +7,24 @@ csv_path = os.path.join('Resources', 'election_data.csv')
 # Initialize variables
 total_votes = 0
 Khan_votes = 0
-Khan_votes0 = 0
+Khan_votes0 = 0.000
 Correy_votes = 0
-Correy_votes0 = 0
+Correy_votes0 = 0.000
 Li_votes = 0
-Li_votes0 = 0
-Tooley_votes0 = 0
+Li_votes0 = 0.000
+Tooley_votes0 = 0.000
 Tooley_votes = 0
 other_votes = 0
-winner = 0
+winner_count = [Khan_votes, Correy_votes, Li_votes, Tooley_votes]
+
+winner_list = ["Khan", "Correy", "Li", "O'Tooley"]
 counter = 0
-
-
+x = 0
+y = 0
 # open and loop data
 with open(csv_path) as File:
     elect_data = csv.reader(File)
-
+    #  loop and tally votes
     for row in elect_data:
         if counter > 0:
             if row[2] == "Khan":
@@ -36,24 +38,26 @@ with open(csv_path) as File:
     
                 
         counter = counter + 1
-    # print(Khan_votes)
-
-
-
-
-
-
+# establish total_votes
+total_votes = counter - 1
+# calculate percentages
+Khan_votes0 = round(((Khan_votes/total_votes) * 100), 3)
+Correy_votes0 = round(((Correy_votes/total_votes) * 100), 3)
+Li_votes0 = round(((Li_votes/total_votes) * 100), 3)
+Tooley_votes0 = round(((Tooley_votes/total_votes) * 100), 3)
+# select winner and match index to winner_list 
+x = max(winner_count)    
 
 # print summary table
 print()
 print(f"Election Results")
 print("---------------------------")
-print(f"Total Votes: {total_votes}")
+print(f"Total Votes: {counter-1}")
 print("---------------------------")
-print(f"Khan: ({Khan_votes})")
-print(f"Correy: ({Correy_votes})")
-print(f"Li: ({Li_votes})")
-print(f"O'Tooley: ({Tooley_votes})")
+print(f"Khan: {Khan_votes0}% ({Khan_votes})")
+print(f"Correy: {Correy_votes0}% ({Correy_votes})")
+print(f"Li: {Li_votes0}% ({Li_votes})")
+print(f"O'Tooley: {Tooley_votes0}% ({Tooley_votes})")
 print("---------------------------")    
-print(f"Winner: {winner}")
+print(f"Winner: {winner_list[x]}")
 print("---------------------------")
